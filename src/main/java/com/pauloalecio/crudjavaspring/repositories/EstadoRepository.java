a9949af7d10efd5518c16e0fae3b1687543d8930
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-03-27 23:39:58 -0300

    Adiciona mettodos a repository

--- src/main/java/com/pauloalecio/crudjavaspring/repositories/EstadoRepository.java
+++ src/main/java/com/pauloalecio/crudjavaspring/repositories/EstadoRepository.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Repository;
 import com.pauloalecio.crudjavaspring.domain.entity.Estado;
 
 
+
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado,Integer>{
-  // Optional<Estado> findBySigla(String sigla);
+  Optional<Estado> findBySigla(String sigla);
+  Optional<Estado>findByNome(String nome);
 }
