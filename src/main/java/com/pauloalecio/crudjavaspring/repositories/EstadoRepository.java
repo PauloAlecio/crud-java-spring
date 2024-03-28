74fc2fca7901262c1b4df256c8ca8c66746ad366
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-03-27 22:30:01 -0300

    Adiciona EstadoRepository a camada de persistencia

---
+++ src/main/java/com/pauloalecio/crudjavaspring/repositories/EstadoRepository.java
@@ -0,0 +1,14 @@
+package com.pauloalecio.crudjavaspring.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pauloalecio.crudjavaspring.domain.entity.Estado;
+
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado,Integer>{
+  // Optional<Estado> findBySigla(String sigla);
+}
