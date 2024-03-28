553731a8e07bd4358aac35bd50864537c50824b4
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-03-27 22:33:02 -0300

    Adiciona método para tratar EstadoException

--- src/main/java/com/pauloalecio/crudjavaspring/exception/handler/ApiExceptionHandler.java
+++ src/main/java/com/pauloalecio/crudjavaspring/exception/handler/ApiExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.pauloalecio.crudjavaspring.exception.EstadoException;
 import com.pauloalecio.crudjavaspring.exception.GeneroException;
 import com.pauloalecio.crudjavaspring.exception.error.ErrorMessage;
 
@@ -22,6 +23,15 @@ public class ApiExceptionHandler {
   
     @ExceptionHandler(GeneroException.class)
     public ResponseEntity<ErrorMessage> handleGeneroException(RuntimeException ex, HttpServletRequest request) {
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, ex.getMessage(), LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(EstadoException.class)
+    public ResponseEntity<ErrorMessage> handleEstadoException(RuntimeException ex, HttpServletRequest request) {
         log.error("Api Error - ", ex);
         return ResponseEntity
             .status(HttpStatus.NOT_FOUND)
@@ -50,6 +60,18 @@ public class ApiExceptionHandler {
             .contentType(MediaType.APPLICATION_JSON)
             .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, message(result),LocalDateTime.now(), result));
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> internalServerErrorException(Exception ex, HttpServletRequest request) {
+        // ErrorMessage error = new ErrorMessage(
+        //         request, HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),LocalDateTime.now());
+        log.error("Api Error - {}", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(),LocalDateTime.now()));
+    }
+
 
     private String message(BindingResult result){
       var message  = String.valueOf(result.getFieldErrorCount() > 2 ? "Campo(s) invalido(s)" : "Campo invalido" );
